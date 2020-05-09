cd82c6d15a7060b64bd7ff10133bcfce6a92300b
masa <mk1988senna.startline@gmail.com>
2020-05-09 13:54:12 +0900

    修正済ぶん

--- class.rb
+++ class.rb
@@ -1,20 +1,21 @@
-class Sword
-    attr_accessor :name
-    attr_accessor :price
+class Menu
+    attr_accessor :name,:price
 
     def initialize (name:,price:)
         self.name = name
         self.price = price
     end
 
-    def menu
+    def lineup
         return "「#{self.name}」 #{self.price}ギル"
     end
 
+    DISCOUNT_TAGET_COUNT = 3
+    DISCOUNT_RATE = 0.8
     def total_price(count)
         total_price_all = self.price*count
-        if count >= 3
-            total_price_all *= 0.8
+        if count >= DISCOUNT_TAGET_COUNT
+            total_price_all *= DISCOUNT_RATE
         end
         return total_price_all
     end
